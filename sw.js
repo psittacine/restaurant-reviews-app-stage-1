2aeccc77ad26a6114e8e0c0e93162e808bb1bcd3
psittacine <psittacine714@gmail.com>
2018-09-22 04:49:02 -0500

    Add Service Worker to cache site assets

---
+++ sw.js
@@ -0,0 +1,62 @@
+const cacheName = 'v1';
+
+const cacheAssets = [
+    '/index.html',
+    '/restaurant.html',
+    '/css/styles.css',
+    '/data/restaurants.json',
+    '/img/1.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/img/10.jpg',
+    '/js/dbhelper.js',
+    '/js/main.js',
+    '/js/restaurant_info.js',
+    '//normalize-css.googlecode.com/svn/trunk/normalize.css',
+    '//unpkg.com/leaflet@1.3.1/dist/leaflet.css'
+];
+
+// Call Install Event
+self.addEventListener('install', e => {
+    console.log('Service Worker: Installed');
+
+    e.waitUntil(
+        caches
+        .open(cacheName)
+        .then(cache => {
+            console.log('Service Worker: Caching Files');
+            cache.addAll(cacheAssets);
+        })
+        .then(() => self.skipWaiting())
+    );
+});
+
+// Call Activate Event
+self.addEventListener('activate', e => {
+    console.log('Service Worker: Activated');
+    // Remove unwanted caches
+    e.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== cacheName) {
+                        console.log('Service Worker: Clearing Old Cache');
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Call Fetch Event
+self.addEventListener('fetch', e => {
+    console.log('Service Worker: Fetching');
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
